0af0c73dc308c0a903ea9267b6cdde078cf4f93b
Komparon <one77@mail.ru>
2022-12-16 04:26:02 +0300

    пока формулу найдешь с объяснениями для гумманитария - поседеешь

---
+++ Dom/dom_5.py
@@ -0,0 +1,10 @@
+#Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+print(100*'\n')
+import math
+x1 = float(input("x1 - "))
+y1 = float(input("y1 - "))
+x2 = float(input("x2 - "))
+y2 = float(input("y2 - "))
+
+result = math.sqrt(((x2-x1)**2+(y2-y1)**2))
+print(result)
